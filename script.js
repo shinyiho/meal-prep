8b0b480b2756aca78c2f973c857ca8ae1e2fe7b9
Glitch (hello-webpage) <none>
2020-11-12 20:49:35 +0000

    🕌👳 Checkpoint ./index.html:11276549/467 ./script.js:11276549/484 ./style.css:11276549/61

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 
+let AvocadoCount = 0
+let LemonCount = 0
+let EggCount = 0
+let BellPepperCount = 0
 
-function myFunction() {
-  document.getElementById("demo").innerHTML = "Hello World";
+function at() {
+  document.getElementById("Avocado").innerHTML = `${AvocadoCount++}`;
+  document.getElementById("Lemon").innerHTML = `${LemonCount++}`;
+  document.getElementById("Egg").innerHTML = `${EggCount++}`;
+  document.getElementById("Bell pepper").innerHTML = `${BellPepperCount++}`;
+  
 }
