ce5845afdf014c354dd0a377204a799d10456080
Glitch (hello-webpage) <none>
2020-12-03 21:52:44 +0000

    😎🙅 Checkpoint ./script.js:11276549/6062

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-
-let AvocadoCount = 0
-let LemonCount = 0
-let EggCount = 0
-let BellPepperCount = 0
-let AvocadoToastCount = 0
+let AvocadoCount = 0;
+let LemonCount = 0;
+let EggCount = 0;
+let BellPepperCount = 0;
+let AvocadoToastCount = 0;
 
 function at() {
   document.getElementById("Avocado").innerHTML = `${AvocadoCount++}`;
@@ -12,25 +11,25 @@ function at() {
   document.getElementById("Bell pepper").innerHTML = `${BellPepperCount++}`;
 }
 
-
-  dragElement(document.getElementById("AvocadoToast0"))
-
+dragElement(document.getElementById("AvocadoToast0"));
 
 function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  console.log("sdhf")
+  var pos1 = 0,
+    pos2 = 0,
+    pos3 = 0,
+    pos4 = 0;
+  console.log("sdhf");
   if (document.getElementById(elmnt.id + "header")) {
     // if present, the header is where you move the DIV from:
     document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
   } else {
     // otherwise, move the DIV from anywhere inside the DIV:
-    
+
     elmnt.onmousedown = dragMouseDown;
-    
   }
 
   function dragMouseDown(e) {
-    console.log(e)
+    console.log(e);
     e = e || window.event;
     e.preventDefault();
     // get the mouse cursor position at startup:
@@ -49,38 +48,38 @@ function dragElement(elmnt) {
     pos2 = pos4 - e.clientY;
     pos3 = e.clientX;
     pos4 = e.clientY;
-    
+
     // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    elmnt.style.top = elmnt.offsetTop - pos2 + "px";
+    elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
     // console.log(pos1,pos2,pos3,pos4)
-    
   }
 
   function closeDragElement() {
     // console.log(elmnt.id)
     // stop moving when mouse button is released:
-    if(elmnt.id===`AvocadoToast${AvocadoToastCount}`){ addRecipe(elmnt)}
+    if (elmnt.id === `AvocadoToast${AvocadoToastCount}`) {
+      addRecipe(elmnt);
+    }
     // console.log(`AvocadoToast${AvocadoToastCount}`)
- 
+
     document.onmouseup = null;
     document.onmousemove = null;
-    
-   
   }
 }
 
-
-
-
-  function addRecipe(elmnt) {
-        console.log(elmnt)
-  var btn = document.createElement("BUTTON");
-  btn.innerHTML = `${elmnt.innerHTML}`;
-  btn.id = `${elmnt.id.slice(0,btn.id.length-1)}${AvocadoToastCount++}`;
-  btn.className = "button AvocadoToast"
-  document.getElementById("recipe").appendChild(btn);
-  dragElement(document.getElementById(`${btn.id}`));
-    console.log(btn.id)
-
+function addRecipe(elmnt) {
+  if (AvocadoToastCount !== 9) {
+    console.log(elmnt);
+    var btn = document.createElement("BUTTON");
+    btn.innerHTML = `${elmnt.innerHTML}`;
+    btn.id = `${elmnt.id.slice(0, btn.id.length - 1)}${++AvocadoToastCount}`;
+    btn.className = "button AvocadoToast";
+    document.getElementById("recipe").appendChild(btn);
+    dragElement(document.getElementById(`${btn.id}`));
+    console.log(btn.id);
+    console.log(AvocadoToastCount);
+  } else {
+    alert(`In order to obtain a balanced diet, ${elmnt.id.slice(0, btn.id.length - 1)}`);
+  }
 }
